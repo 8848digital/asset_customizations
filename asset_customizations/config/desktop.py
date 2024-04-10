1017b1e9825022a231f6188e3480ed3fdbe375d7
Amandeep8848 <eight848-digital@DESKTOP-ARCF17F>
2024-04-10 18:54:44 +0530

    feat: added pre-commits and structured the existing code accordingly

--- asset_customizations/config/desktop.py
+++ asset_customizations/config/desktop.py
@@ -1,10 +1,5 @@
 from frappe import _
 
+
 def get_data():
-	return [
-		{
-			"module_name": "asset_modification",
-			"type": "module",
-			"label": _("asset_modification")
-		}
-	]
+	return [{"module_name": "asset_modification", "type": "module", "label": _("asset_modification")}]
